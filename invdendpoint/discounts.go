ca2c0be7907a6424d4a5389b2f1cf0f90391dfe1
Parag Patel <parag.patel.one@gmail.com>
2016-03-15 20:38:14 -0500

    Changed Discount->Expires to handle null

--- invdendpoint/discounts.go
+++ invdendpoint/discounts.go
@@ -4,5 +4,5 @@ type Discount struct {
 	Id      int64   `json:"id,omitempty"`
 	Amount  float64 `json:"amount,omitempty"`
 	Coupon  Rate    `json:"coupon,omitempty"`
-	Expires int64   `json:"expires,omitempty"`
+	Expires *int64  `json:"expires,omitempty"`
 }
