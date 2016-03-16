38cd6787a4ad5658812de75e70583b628eb86c91
Parag Patel <parag.patel.one@gmail.com>
2016-03-15 22:12:52 -0500

    Changed Rate id from int to string

--- invdendpoint/discounts.go
+++ invdendpoint/discounts.go
@@ -1,8 +1,8 @@
 package invdendpoint
 
 type Discount struct {
-	Id     int64   `json:"id,omitempty"`
-	Amount float64 `json:"amount,omitempty"`
-	// Coupon  Rate    `json:"coupon,omitempty"`
-	Expires *int64 `json:"expires,omitempty"`
+	Id      int64   `json:"id,omitempty"`
+	Amount  float64 `json:"amount,omitempty"`
+	Coupon  Rate    `json:"coupon,omitempty"`
+	Expires *int64  `json:"expires,omitempty"`
 }
--- invdendpoint/rates.go
+++ invdendpoint/rates.go
@@ -5,7 +5,7 @@ const RatesEndPoint = "/rates/"
 type Rate struct {
 	CreatedAt int64       `json:"created_at,omitempty"`
 	UpdatedAt int64       `json:"updated_at,omitempty"`
-	Id        int64       `json:"id,omitempty"`
+	Id        string      `json:"id,omitempty"`
 	IsPercent bool        `json:"is_percent,omitempty"`
 	MetaData  interface{} `json:"metadata,omitempty"`
 	Name      string      `json:"name,omitempty"`
