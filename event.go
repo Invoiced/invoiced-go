6c27805660f72637a5dccd83ea6f56ad7f898c93
Parag Patel <parag.patel.one@gmail.com>
2016-10-08 11:11:19 -0500

    Deprecated updated_at in invoice endpoint.  Added listing methods of events.

---
+++ event.go
@@ -0,0 +1,69 @@
+package invdapi
+
+import (
+	"github.com/Invoiced/invoiced-go/invdendpoint"
+)
+
+type Event struct {
+	*Connection
+	*invdendpoint.Event
+}
+
+type Events []*Event
+
+func (c *Connection) NewEvent() *Event {
+	event := new(invdendpoint.Event)
+	return &Event{c, event}
+
+}
+
+func (c *Event) ListAll(filter *invdendpoint.Filter, sort *invdendpoint.Sort) (Events, error) {
+	endPoint := c.makeEndPointURL(invdendpoint.EventsEndPoint)
+	endPoint = addFilterSortToEndPoint(endPoint, filter, sort)
+
+	events := make(Events, 0)
+
+NEXT:
+	tmpEvents := make(Events, 0)
+
+	endPoint, apiErr := c.retrieveDataFromAPI(endPoint, &tmpEvents)
+
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	events = append(events, tmpEvents...)
+
+	if endPoint != "" {
+		goto NEXT
+	}
+
+	for _, event := range events {
+		event.Connection = c.Connection
+
+	}
+
+	return events, nil
+
+}
+
+func (c *Event) List(filter *invdendpoint.Filter, sort *invdendpoint.Sort) (Events, string, error) {
+	endPoint := c.makeEndPointURL(invdendpoint.EventsEndPoint)
+	endPoint = addFilterSortToEndPoint(endPoint, filter, sort)
+
+	events := make(Events, 0)
+
+	nextEndPoint, apiErr := c.retrieveDataFromAPI(endPoint, &events)
+
+	if apiErr != nil {
+		return nil, "", apiErr
+	}
+
+	for _, event := range events {
+		event.Connection = c.Connection
+
+	}
+
+	return events, nextEndPoint, nil
+
+}
--- invdendpoint/events.go
+++ invdendpoint/events.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 )
 
+const EventsEndPoint = "/events"
+
 type Event struct {
-	Id   int64           `json:"id,omitempty"`   //The event’s unique ID
-	Type string          `json:"type,omitempty"` //Event type
-	Data json.RawMessage `json:"data,omitempty"` //Contains an object property with the object that was subject of the event and an optional previous property for object.updated events that is a hash of the old values that changed during the event
+	Id        int64           `json:"id,omitempty"`   //The event’s unique ID
+	Type      string          `json:"type,omitempty"` //Event type
+	Timestamp int64           `json:"timestamp,omitempty"`
+	Data      json.RawMessage `json:"data,omitempty"` //Contains an object property with the object that was subject of the event and an optional previous property for object.updated events that is a hash of the old values that changed during the event
 }
