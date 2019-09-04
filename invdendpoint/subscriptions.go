0047c2811a271ecca7b35c67a712628f0733f7c9
Shea Newton <shnewto@gmail.com>
2019-09-04 09:57:34 -0700

    Add ShipTo field to Subscription struct (#13)
    * Add ShipTo field to Subscription
    
    Prior to this commit, if a user wanted to add a shipping address to a
    subscription, they were required to make a PATCH or POST request
    direclty to the Invoiced REST API.
    
    This can cause issues because creating and updating subscriptions with shipping
    addresses requires logic for that field alone to be separate from the
    rest of the subscription handling, i.e. more code to maintain and more
    places for things to fail.
    
    Thanks for taking a look!
    
    * Add contract_renewal_mode and prorate to subscription object

--- invdendpoint/subscriptions.go
+++ invdendpoint/subscriptions.go
@@ -24,6 +24,18 @@ type Subscription struct {
 	Url         string                 `json:"url,omitempty"`          //URL to manage the subscription in the billing portal
 	CreatedAt   int64                  `json:"created_at,omitempty"`   //Timestamp when created
 	MetaData    map[string]interface{} `json:"metadata,omitempty"`     //A hash of key/value pairs that can store additional information about this object.
+	Prorate bool `json:"prorate,omitempty"`
+	ContractRenewalMode string `json:"contract_renewal_mode,omitempty"`
+	ShipTo      struct {
+		Address1    string `json:"address1,omitempty"`
+		Address2    string `json:"address2,omitempty"`
+		AttentionTo string `json:"attention_to,omitempty"`
+		City        string `json:"city,omitempty"`
+		Country     string `json:"country,omitempty"`
+		Name        string `json:"name,omitempty"`
+		PostalCode  string `json:"postal_code,omitempty"`
+		State       string `json:"state,omitempty"`
+	} `json:"ship_to,omitempty"` // Shipping address
 }
 
 func (s *Subscription) String() string {
