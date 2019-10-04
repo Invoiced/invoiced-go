4498f8e3de42880c56294a90cadf88672d2f8301
Adam Train <adamtrain@fastmail.com>
2019-10-04 13:16:14 -0500

    Update invoices_test.go - CatalogItem is string

--- invdendpoint/invoices_test.go
+++ invdendpoint/invoices_test.go
@@ -43,21 +43,7 @@ func TestUnMarshalInvoiceObject(t *testing.T) {
     },
     {
       "id": 8,
-      "catalog_item": {
-  "id": "delivery",
-  "object": "catalog_item",
-  "name": "Delivery",
-  "currency": "usd",
-  "unit_cost": 100,
-  "description": null,
-  "type": "service",
-  "taxes": [],
-  "discountable": true,
-  "taxable": true,
-  "unit_cost": 10,
-  "created_at": 1477327516,
-  "metadata": {}
-},
+      "catalog_item": "{\"id\":\"delivery\"}",
       "type": "service",
       "name": "Delivery",
       "description": "",
