d12dc61558c4ab20a6a84d68913dcd7bbc3a165b
Parag Patel <parag.patel.one@gmail.com>
2020-02-20 11:22:36 -0600

    make invoice.draft manditory, and remove omitempty.  bumping to version 5.0.5

--- invdendpoint/invoices.go
+++ invdendpoint/invoices.go
@@ -14,7 +14,7 @@ type Invoice struct {
 	Number                 string                 `json:"number,omitempty"`               //The reference number assigned to the invoice for use in the dashboard
 	AutoPay                bool                   `json:"autopay,omitempty"`              //Invoice collection mode, auto or manual
 	Currency               string                 `json:"currency,omitempty"`             //3-letter ISO code
-	Draft                  bool                   `json:"draft,omitempty"`                //When false, the invoice is considered outstanding, or when true, the invoice is a draft
+	Draft                  bool                   `json:"draft"`                          //When false, the invoice is considered outstanding, or when true, the invoice is a draft
 	Closed                 bool                   `json:"closed,omitempty"`               //When true, an invoice is closed and considered bad debt. No further payments are allowed.
 	Paid                   bool                   `json:"paid,omitempty"`                 //Indicates whether an invoice has been paid in full
 	Status                 string                 `json:"status,omitempty"`               //Invoice state, one of draft, not_sent, sent, viewed, past_due, pending, paid
