38fc59ed6caf2e0c54891552766d7a149ca2b944
Parag Patel <parag.patel.one@gmail.com>
2020-04-28 17:52:50 -0500

    add template to emailrequest; bump version to 5.4.3

--- connector.go
+++ connector.go
@@ -17,7 +17,7 @@ const devRequestURL = "https://api.sandbox.invoiced.com"
 const requestType = "application/json"
 const InvoicedTokenString = "invoicedToken"
 
-const version = "5.4.2"
+const version = "5.4.3"
 
 func Version() string {
 	return version
--- invdendpoint/emails.go
+++ invdendpoint/emails.go
@@ -1,14 +1,15 @@
 package invdendpoint
 
 type EmailRequest struct {
-	To      []EmailDetail `json:"to,omitempty"`
-	Bcc     string        `json:"bcc,omitempty"`
-	Subject string        `json:"subject,omitempty"`
-	Message string        `json:"message,omitempty"`
-	Type    string        `json:"type,omitempty"`
-	Start   int64         `json:"start,omitempty"`
-	End     int64         `json:"end,omitempty"`
-	Items   string        `json:"items,omitempty"`
+	To       []EmailDetail `json:"to,omitempty"`
+	Bcc      string        `json:"bcc,omitempty"`
+	Subject  string        `json:"subject,omitempty"`
+	Message  string        `json:"message,omitempty"`
+	Template string        `json:"template,omitempty"`
+	Type     string        `json:"type,omitempty"`
+	Start    int64         `json:"start,omitempty"`
+	End      int64         `json:"end,omitempty"`
+	Items    string        `json:"items,omitempty"`
 }
 
 type EmailDetail struct {
