4d27cade3e963c49e25570dd03b698bc26b61c5a
Parag Patel <parag.patel.one@gmail.com>
2020-07-19 20:48:10 -0500

    estimate object change approved from boolean to string in response to api changes

--- connector.go
+++ connector.go
@@ -17,7 +17,7 @@ const devRequestURL = "https://api.sandbox.invoiced.com"
 const requestType = "application/json"
 const InvoicedTokenString = "invoicedToken"
 
-const version = "5.4.6"
+const version = "5.4.7"
 
 func Version() string {
 	return version
