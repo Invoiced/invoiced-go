5e1d44d33e356298efe92f5a9b6fd56dd384277a
Parag Patel <parag.patel.one@gmail.com>
2021-02-04 22:29:34 -0600

    add user logic

--- invdendpoint/users.go
+++ invdendpoint/users.go
@@ -1,5 +1,28 @@
 package invdendpoint
 
+const UsersEndpoint = "/members"
+
+type UserRequests []UserRequest
+
+type UserRequest struct {
+	Email           string              `json:"email,omitempty"`
+	FirstName       string              `json:"first_name,omitempty"`
+	LastName        string              `json:"last_name,omitempty"`
+	Role            string              `json:"role,omitempty"`
+	RestrictionMode string              `json:"restriction_mode,omitempty"`
+	Restrictions    map[string][]string `json:"restrictions,omitempty"`
+}
+
+type UserResponse struct {
+	CreatedAt            int64               `json:"created_at,omitempty"`
+	EmailUpdateFrequency string              `json:"email_update_frequency,omitempty"`
+	Id                   int64               `json:"id,omitempty"`
+	RestrictionMode      string              `json:"restriction_mode,omitempty"`
+	Restrictions         map[string][]string `json:"restrictions,omitempty"`
+	Role                 string              `json:"role,omitempty"`
+	User                 *User               `json:"user,omitempty"`
+}
+
 type User struct {
 	Email            string `json:"email,omitempty"`
 	FirstName        string `json:"first_name,omitempty"`
---
+++ user.go
@@ -0,0 +1,108 @@
+package invdapi
+
+import (
+	"strconv"
+
+	"github.com/Invoiced/invoiced-go/invdendpoint"
+)
+
+type User struct {
+	*Connection
+	*invdendpoint.UserResponse
+}
+
+type Users []*User
+
+func (c *Connection) NewUser() *User {
+	user := new(invdendpoint.UserResponse)
+	return &User{c, user}
+}
+
+func (c *User) Create(userRequest *invdendpoint.UserRequest) (*User, error) {
+	endpoint := invdendpoint.UsersEndpoint
+
+	userResp := new(User)
+
+	apiErr := c.create(endpoint, userRequest, userResp)
+
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	userResp.Connection = c.Connection
+
+	return userResp, nil
+}
+
+func (c *User) Save(userRequest *invdendpoint.UserRequest, id int64) error {
+	endpoint := invdendpoint.UsersEndpoint + "/" + strconv.FormatInt(id, 10)
+
+	userResp := new(User)
+
+	apiErr := c.update(endpoint, userRequest, userResp)
+
+	if apiErr != nil {
+		return apiErr
+	}
+
+	userResp.Connection = c.Connection
+
+	return nil
+}
+
+func (c *User) Delete(id int64) error {
+	endpoint := invdendpoint.UsersEndpoint + "/" + strconv.FormatInt(id, 10)
+
+	err := c.delete(endpoint)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *User) Retrieve(id int64) (*User, error) {
+	endpoint := invdendpoint.UsersEndpoint + "/" + strconv.FormatInt(id, 10)
+
+	userResp := new(User)
+
+	_, err := c.retrieveDataFromAPI(endpoint, userResp)
+	if err != nil {
+		return nil, err
+	}
+
+	userResp.Connection = c.Connection
+
+	return userResp, nil
+}
+
+func (c *User) ListAll(filter *invdendpoint.Filter, sort *invdendpoint.Sort) (Users, error) {
+	endpoint := invdendpoint.UsersEndpoint
+
+	endpoint = addFilterAndSort(endpoint, filter, sort)
+
+	users := make(Users, 0)
+
+NEXT:
+	tmpUsers := make(Users, 0)
+
+	endpointTmp, apiErr := c.retrieveDataFromAPI(endpoint, &tmpUsers)
+
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	users = append(users, tmpUsers...)
+
+	if endpointTmp != "" {
+		goto NEXT
+	}
+
+	for _, user := range users {
+		user.Connection = c.Connection
+	}
+
+	return users, nil
+}
+
+
