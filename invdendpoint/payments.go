24e32c43817410471a468ccee204a15dbd8d7ec9
Parag Patel <parag.patel.one@gmail.com>
2021-04-28 12:55:35 -0500

    payment object support full and short customer

--- invdendpoint/payments.go
+++ invdendpoint/payments.go
@@ -1,5 +1,10 @@
 package invdendpoint
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 const PaymentEndpoint = "/payments"
 
 type Payments []Payment
@@ -7,7 +12,9 @@ type Payments []Payment
 type Payment struct {
 	Id        int64         `json:"id,omitempty"`       // The payment’s unique ID
 	Object    string        `json:"object,omitempty"`   // Object type, payment
-	Customer  int64         `json:"customer,omitempty"` // Customer ID, required if invoice ID is not supplied
+	Customer               int64                  `json:"-"`
+	CustomerFull           *Customer              `json:"-"`
+	CustomerRaw             json.RawMessage        `json:"customer,omitempty"`
 	Date      int64         `json:"date,omitempty"`     // Payment date, defaults to current timestamp
 	Method    string        `json:"method,omitempty"`   // Payment instrument used to facilitate payment, defaults to other
 	Matched   bool          `json:"matched,omitempty"`
@@ -32,4 +39,43 @@ type PaymentItem struct {
 	Estimate     int64   `json:"estimate,omitempty"`
 	DocumentType int64   `json:"document_type,omitempty"`
 	Amount       float64 `json:"amount,omitempty"`
+}
+
+func (i *Payment) UnmarshalJSON(data []byte) error {
+	type payment2 Payment
+
+	if err := json.Unmarshal(data, (*payment2)(i)); err != nil {
+		return err
+	}
+
+	rj := i.CustomerRaw
+
+	i.Customer, _ = strconv.ParseInt(string(rj), 10, 64)
+	customer := new(Customer)
+
+	err := json.Unmarshal(rj, customer)
+
+	if err == nil {
+		i.CustomerFull = customer
+		i.Customer = customer.Id
+	}
+
+	return nil
+}
+
+func (i *Payment) MarshalJSON() ([]byte, error) {
+	type payment2 Payment
+	i2 := (*payment2)(i)
+
+	if i2.Customer > 0 {
+		i2.CustomerRaw = []byte(strconv.FormatInt(i2.Customer, 10))
+	}
+
+	return json.Marshal(i2)
+}
+
+func (i *Payment) String() string {
+	b, _ := json.MarshalIndent(i, "", "    ")
+
+	return string(b)
 }
