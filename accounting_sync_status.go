5e6279c177eafc4ea64768ca30d59652e27ff1b1
Parag Patel <parag.patel.one@gmail.com>
2022-03-23 17:08:43 -0500

    clean up accounting sync status

--- accounting_sync_status.go
+++ accounting_sync_status.go
@@ -2,11 +2,13 @@ package invoiced
 
 
 type AccountingSyncStatus struct {
-	AccountingId     string `json:"accounting_id"`
-	AccountingSystem string `json:"accounting_system"`
+	Synced           bool   `json:"synced"`
 	Error            string `json:"error"`
+	AccountingSystem string `json:"accounting_system"`
+	AccountingId     string `json:"accounting_id"`
+	Source           string `json:"source"`
 	FirstSynced      int64  `json:"first_synced"`
 	LastSynced       int64  `json:"last_synced"`
-	Source           string `json:"source"`
-	Synced           bool   `json:"synced"`
+
+
 }
